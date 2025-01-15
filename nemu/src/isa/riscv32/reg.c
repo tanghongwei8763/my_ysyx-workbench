890c67281240dc93ea19d26756aa604e871a17ee
tracer-ysyx <tracer@ysyx.org>
2025-01-15 16:04:41 +0800

    > compile NEMU ysyx_202401201017 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:04:41 up  2:07,  1 user,  load average: 0.06, 0.05, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,6 +24,7 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+  printf("name\t\t0x16\td10\n");
   for (int i = 0; i < sizeof(regs)/sizeof(regs[0]); i++)
     printf("%s\t\t0x%08x\t%d\n", regs[i], *regs[i], *regs[i]);
 }
