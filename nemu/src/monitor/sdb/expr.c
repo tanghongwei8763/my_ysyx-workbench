ba7fb6415ee1ebea9f2ecba4a902b32f670086d1
tracer-ysyx <tracer@ysyx.org>
2025-01-17 16:47:02 +0800

    > compile NEMU ysyx_202401201017 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:47:02 up  2:42,  1 user,  load average: 0.12, 0.06, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -139,6 +139,7 @@ static bool make_token(char *e) {
 	    tokens[nr_token].type = TK_DOLLAR;
 	    tokens[nr_token].pri = 4;
 	    int j = 0;
+	    pos++;
 	    printf("$已存入\n");
 	    while ((e[pos]>='0'&&e[pos]<='9') || (e[pos]>='a'&&e[pos]<='z') || (e[pos]>='A'&&e[pos]<='Z')) {
 	      printf("$进了while循环\n");
