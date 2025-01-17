e786bf8f292e28bcb66cc278d35b6d11f924528b
tracer-ysyx <tracer@ysyx.org>
2025-01-17 20:35:52 +0800

    > compile NEMU ysyx_202401201017 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:35:52 up 29 min,  1 user,  load average: 0.00, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -178,11 +178,11 @@ static bool make_token(char *e) {
       return false;
     }
   }
-  /*
+  
   for(int ert = 0; ert < nr_token; ert++) {		//检测点
     printf("%c\t%s\n", tokens[ert].type, tokens[ert].str);
   }
-  */
+  
   return true;
 }
 
