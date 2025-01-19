4b81c8c47649ffed75db0e8a7701023083a6e56c
tracer-ysyx <tracer@ysyx.org>
2025-01-19 14:19:58 +0800

    > compile NEMU ysyx_202401201017 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:19:58 up 20 min,  1 user,  load average: 0.00, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -163,7 +163,7 @@ static bool make_token(char *e) {
               break;
             }
           }
-          case '/': tokens[nr_token].type = '/';tokens[nr_token].pri = 2;nr_token++;pos++;break;
+          case '/': printf("%d\n", position);tokens[nr_token].type = '/';tokens[nr_token].pri = 2;nr_token++;pos++;break;
           case TK_LPAREN: tokens[nr_token].type = TK_LPAREN;tokens[nr_token].pri = 2;nr_token++;pos++;break;
 	  case TK_RPAREN: tokens[nr_token].type = TK_RPAREN;tokens[nr_token].pri = 0;nr_token++;pos++;break;
 	  case TK_EQ: tokens[nr_token].type = TK_EQ;tokens[nr_token].pri = 3;nr_token++;pos+=2;break;
