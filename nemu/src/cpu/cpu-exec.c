ede907913bd2bc647f7b3fc37b3d1f4e8743e6f7
tracer-ysyx <tracer@ysyx.org>
2025-01-20 09:21:41 +0800

    > compile NEMU ysyx_202401201017 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:21:41 up 18 min,  1 user,  load average: 0.04, 0.03, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -46,6 +46,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 #ifdef CONFIG_WATCHPOINT			//监视点
+  printf("enter(0)\n");
   void watchpoint_exec();
 #endif
 }
