e99c49bed02c06e90e450e343068afafe6df9a8f
tracer-ysyx <tracer@ysyx.org>
2025-02-15 17:17:20 +0800

    > compile NEMU ysyx_202401201017 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:17:20 up  5:56,  1 user,  load average: 0.17, 0.05, 0.01

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -58,6 +58,7 @@ static int decode_exec(Decode *s) {
   int rd = 0; \
   word_t src1 = 0, src2 = 0, imm = 0; \
   decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
+  printf("imm=%d\n", imm);\
   __VA_ARGS__ ; \
 }
 
