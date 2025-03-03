62f4d3c19b9959b1ddc5fdd94dba3b68f00e5f70
tracer-ysyx <tracer@ysyx.org>
2025-03-03 19:18:50 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:18:50 up 4 min,  1 user,  load average: 0.06, 0.14, 0.08

---
+++ npc/obj_dir/Vysyx_25020037_Reg___024root__Slow.cpp
@@ -0,0 +1,25 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vysyx_25020037_Reg.h for the primary calling header
+
+#include "verilated.h"
+
+#include "Vysyx_25020037_Reg__Syms.h"
+#include "Vysyx_25020037_Reg___024root.h"
+
+void Vysyx_25020037_Reg___024root___ctor_var_reset(Vysyx_25020037_Reg___024root* vlSelf);
+
+Vysyx_25020037_Reg___024root::Vysyx_25020037_Reg___024root(Vysyx_25020037_Reg__Syms* symsp, const char* v__name)
+    : VerilatedModule{v__name}
+    , vlSymsp{symsp}
+ {
+    // Reset structure values
+    Vysyx_25020037_Reg___024root___ctor_var_reset(this);
+}
+
+void Vysyx_25020037_Reg___024root::__Vconfigure(bool first) {
+    if (false && first) {}  // Prevent unused
+}
+
+Vysyx_25020037_Reg___024root::~Vysyx_25020037_Reg___024root() {
+}
