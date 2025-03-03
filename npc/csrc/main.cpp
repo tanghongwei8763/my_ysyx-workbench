0c1c7cbd915e77e94727d55efa0e5b5ddca2ed5a
tracer-ysyx <tracer@ysyx.org>
2025-03-03 19:51:36 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:51:36 up 37 min,  1 user,  load average: 0.08, 0.13, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -17,17 +17,22 @@ int main() {
 
 
 // 设置时钟，复位信号
-void single_cycle(Vysyx_25020037_cpu& dut) {
-    dut.clk=1;
-    dut.eval();
+void single_cycle(Vysyx_25020037_cpu& dut, VerilatedContext* contextp, VerilatedVcdC* tfp) {
     dut.clk=0;
     dut.eval();
+    tfp->dump(contextp->time());
+    contextp->timeInc(1);
+
+    dut.clk=1;
+    dut.eval();
+    tfp->dump(contextp->time());
+    contextp->timeInc(1);
 }
 
-static void reset(Vysyx_25020037_cpu& dut, int n) {
-    dut.rst = 0;
-    while (n -- > 0) single_cycle(dut);
+static void reset(Vysyx_25020037_cpu& dut, VerilatedContext* contextp, VerilatedVcdC* tfp, int n) {
     dut.rst = 1;
+    while (n -- > 0) single_cycle(dut, contextp, tfp);
+    dut.rst = 0;
 }
 
 int main (int argc, char** argv)
@@ -49,9 +54,7 @@ int main (int argc, char** argv)
     reset(dut,10);
     for(int i=0; i < 6; i++){
         dut.inst = pmem_read(4*i+0x80000000, 4);
-        single_cycle(dut);
-        tfp->dump(contextp->time());
-        contextp->timeInc(1);
+        single_cycle(dut, contextp, tfp);
     }
 
     tfp->close();
