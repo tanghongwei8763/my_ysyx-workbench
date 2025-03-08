e41abb06ce92149be948e2dcfb8619d0f71d5d37
tracer-ysyx <tracer@ysyx.org>
2025-03-08 13:28:29 +0800

    > compile NEMU ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:28:29 up 43 min,  1 user,  load average: 0.00, 0.02, 0.04

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,15 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  char *temp = dst;
+  while (*src) {
+    *temp = *src;
+    temp++;
+    src++;
+  }
+  *temp = '\0';
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +25,32 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  char *temp = dst;
+  while (*temp) {
+    temp++;
+  }
+  while (*src) {
+    *temp = *src;
+    temp++;
+    src++;
+  }
+  *temp = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  while (*s1 && *s2 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+  if(*s1 < *s2)
+    return -1;
+  else if (*s1 > *s2)
+    return 1;
+  else 
+    return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +58,13 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  unsigned char *temp = (unsigned char *)s;
+  for (size_t i = 0; i < n; i++) {
+    *temp = c;
+    temp ++;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +76,19 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+    const unsigned char *temp1 = (const unsigned char *)s1;
+    const unsigned char *temp2 = (const unsigned char *)s2;
+  while (n) {
+    if(*temp1 < *temp2)
+      return -1;
+    else if (*temp1 > *temp2)
+      return 1;
+    temp1++;
+    temp2++;
+    n--;
+  }
+  return 0;
 }
 
 #endif
