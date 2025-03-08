20f2eb8d262bd76a4601d39cfb918e4c56ea5c83
tracer-ysyx <tracer@ysyx.org>
2025-03-08 20:53:51 +0800

    > compile NEMU ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:53:51 up  2:02,  1 user,  load average: 0.04, 0.09, 0.08

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,6 +13,37 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+void inttostr(int num, char *str, int base) {
+    int i = 0;
+    int is_negative = 0;
+    if (num < 0 && base == 10) {
+        is_negative = 1;
+        num = -num;
+    }
+    if (num == 0) {
+        str[i++] = '0';
+    }
+    while (num != 0) {
+        int rem = num % base;
+        str[i++] = (rem > 9)? (rem - 10) + 'a' : rem + '0';
+        num = num / base;
+    }
+    if (is_negative) {
+        str[i++] = '-';
+    }
+    str[i] = '\0';
+
+    int start = 0;
+    int end = i - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
 int sprintf(char *out, const char *fmt, ...) {
   //panic("Not implemented");
   va_list args;
@@ -32,25 +63,15 @@ int sprintf(char *out, const char *fmt, ...) {
           break;
         }
         case 'd': {
-          int num = va_arg(args, int);
-          int dnum = num, ynum;//十位以上的数（整除后的）和余数
-          if(num < 0) {
-            num = -num;
-            out[temp] = '-';
-            temp++;
-          }
-          else if (num == 0) {
-            out[temp] = '0';
-            temp++;
-          }
-          while(dnum) {
-            ynum = dnum % 10;
-            dnum = dnum / 10;
-            out[temp] = ynum + '0';
-            temp++;
-          }
-          break;
-        }
+                    int num = va_arg(args, int);
+                    char num_str[20];
+                    inttostr(num, num_str, 10);
+                    int len = strlen(num_str);
+                    for (int j = 0; j < len; j++) {
+                        out[temp++] = num_str[j];
+                    }
+                    break;
+                }
         default:{
           out[temp++] = '%';
           out[temp++] = fmt[i];
