fdfe5ecc348609f354d374f01b710040b22cffac
tracer-ysyx <tracer@ysyx.org>
2025-03-08 20:45:01 +0800

    > compile NEMU ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:45:01 up  1:54,  1 user,  load average: 0.07, 0.07, 0.08

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,50 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+  int temp = 0;
+  for(int i = 0; fmt[i] != '\0'; i++) {
+    if(fmt[i] == '%') {
+      i++;
+      switch (fmt[i]) {
+        case 's':{
+          char *str = va_arg(args, char *);
+          int len = strlen(str);
+          for(int j = 0; j < len; j++) {
+            out[temp] = str[j];
+            temp++;
+          }
+        }
+        case 'd': {
+          int num = va_arg(args, int);
+          int dnum = 1, ynum;//十位以上的数（整除后的）和余数
+          if(num < 0) {
+            out[temp] = '-';
+            temp++;
+          }
+          while(dnum) {
+            dnum = num / 10;
+            ynum = num % 10;
+            out[temp] = ynum + '0';
+            temp++;
+          }
+        }
+        default:{
+          out[temp++] = '%';
+          out[temp++] = fmt[i];
+        }
+      }
+    }
+    else {
+      out[temp] = fmt[i];
+      temp++;
+    }
+  }
+  out[temp] = '\0';
+  va_end(args);
+  return temp;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,7 +5,12 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t len = 0;
+  while(*s) {
+    len++;
+  }
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
