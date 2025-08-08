004e40c97b7c1cebe9866bec44ae6b56058bee3c
tracer-ysyx <tracer@ysyx.org>
2025-08-08 15:41:39 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:41:39 up  4:41,  1 user,  load average: 0.25, 0.13, 0.09

--- abstract-machine/am/src/riscv/ysyxsoc/ioe.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe.c
@@ -6,6 +6,7 @@ void __am_timer_init();
 void __am_timer_rtc(AM_TIMER_RTC_T *);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *);
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *);
+void __am_uart_rx(AM_UART_RX_T *);
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
@@ -18,7 +19,8 @@ static void *lut[128] = {
   [AM_TIMER_UPTIME] = __am_timer_uptime,
   [AM_INPUT_CONFIG] = __am_input_config,
   [AM_INPUT_KEYBRD] = __am_input_keybrd,
-  [AM_UART_CONFIG]  = __am_uart_config,
+  [AM_UART_CONFIG ] = __am_uart_config,
+  [AM_UART_RX     ] = __am_uart_rx
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
