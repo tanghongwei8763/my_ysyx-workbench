45b7e5ba21cc3464360a2f32f8b8533c8f945564
tracer-ysyx <tracer@ysyx.org>
2025-08-11 16:51:20 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:51:20 up  3:26,  1 user,  load average: 0.22, 0.11, 0.04

--- npc/vsrc/core-npc/ysyx_25020037_ifu.v
+++ npc/vsrc/core-npc/ysyx_25020037_ifu.v
@@ -16,52 +16,11 @@ module ysyx_25020037_ifu(
     input  wire         rvalid,
     output reg          rready
 );
-    localparam IDLE    = 2'b00;
-    localparam CHECK   = 2'b01;
-    localparam BUSY    = 2'b10;
-    localparam WAIT    = 2'b11;
+    localparam IDLE    = 1'b0;
+    localparam BUSY    = 1'b1;
     
-    reg  [ 1:0] state, next_state;
+    reg         state, next_state;
     reg  [31:0] last_pc;
-    wire [31:0] icache_data;
-    wire        icache_hit;
-    wire        icache_ready;
-    reg         icache_req;
-    reg  [31:0] mem_addr_to_cache;
-    reg         mem_req_to_cache;
-    wire [31:0] mem_data_from_cache;
-    wire        mem_ready_from_cache;
-
-    ysyx_25020037_icache #(
-        .ADDR_WIDTH    (32),
-        .DATA_WIDTH    (32),
-        .CACHE_BLOCKS  (16),
-        .BLOCK_SIZE    (4 )
-    ) u_icache (
-        .clk           (clk         ),
-        .rst           (rst         ),
-        .cpu_addr      (pc          ),
-        .cpu_req       (icache_req  ),
-        .cpu_data      (icache_data ),
-        .cpu_hit       (icache_hit  ),
-        .cpu_ready     (icache_ready),
-        .mem_addr      (mem_addr_to_cache   ),
-        .mem_req       (mem_req_to_cache    ),
-        .mem_data      (rdata               ),
-        .mem_ready     (mem_ready_from_cache)
-    );
-    assign mem_ready_from_cache = rvalid && (rresp == 2'b00);
-
-    always @(*) begin
-        case (state)
-            IDLE: begin next_state = (pc != last_pc) ? CHECK : IDLE; end
-            CHECK: begin next_state = icache_hit ? IDLE : BUSY; end
-            BUSY: begin next_state = (rvalid && rready && (rresp == 2'b00)) ? WAIT : BUSY; end
-            WAIT: begin next_state = icache_ready ? IDLE : WAIT; end
-            default: next_state = IDLE;
-        endcase
-    end
-
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             state <= IDLE;
@@ -70,51 +29,39 @@ module ysyx_25020037_ifu(
             arvalid <= 1'b0;
             rready <= 1'b0;
             last_pc <= 32'h0;
-            icache_req <= 1'b0;
-            mem_req_to_cache <= 1'b0;
-            mem_addr_to_cache <= 32'h0;
         end else begin
             state <= next_state;
             
             case (state)
                 IDLE: begin
-                    if (pc != last_pc) begin
-                        last_pc <= pc;
-                        icache_req <= 1'b1;
-                    end else begin
-                        icache_req <= 1'b0;
-                    end
-                    arvalid <= 1'b0;
-                    rready <= 1'b0;
-                end
-                CHECK: begin
-                    icache_req <= 1'b0;
-                    if (icache_hit) begin
-                        inst <= icache_data;
-                    end else begin
-                        araddr <= pc;
+                    if ((pc != last_pc)) begin
+                        araddr  <= pc;
                         arvalid <= 1'b1;
-                        mem_addr_to_cache <= pc;
-                        mem_req_to_cache <= 1'b1;
+                        last_pc <= pc;
+                        rready  <= 1'b0;
                     end
                 end
                 BUSY: begin
                     if (arvalid && arready) begin
+                        //araddr  <= 32'b0;
                         arvalid <= 1'b0;
                         rready <= 1'b1;
                     end
                     if (rvalid && rready && (rresp == 2'b00)) begin
+                        inst <= rdata;
                         rready <= 1'b0;
-                        mem_req_to_cache <= 1'b0;
-                    end
-                end
-                WAIT: begin
-                    if (icache_ready) begin
-                        inst <= icache_data;
                     end
                 end
             endcase
         end
+    end
+
+    always @(*) begin
+        case (state)
+            IDLE: next_state = (pc != araddr) ? BUSY : IDLE;        
+            BUSY: next_state = (rvalid && rready) ? IDLE : BUSY;    
+            default: next_state = IDLE;
+        endcase
     end
 
 endmodule
