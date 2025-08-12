c3fa2efdc0141abf110d6a2ef88ecd115b9191cf
tracer-ysyx <tracer@ysyx.org>
2025-08-12 15:30:00 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:30:00 up 12:31,  1 user,  load average: 0.35, 0.47, 0.31

--- npc/vsrc/core-soc/ysyx_25020037_ifu.v
+++ npc/vsrc/core-soc/ysyx_25020037_ifu.v
@@ -5,7 +5,6 @@ module ysyx_25020037_ifu(
     input  wire         rst,
     input  wire [31: 0] pc,
     input  wire         idu_ready,
-    output reg          ifu_valid,
     output reg  [31: 0] inst,
     output reg          access_fault,
 
@@ -21,34 +20,13 @@ module ysyx_25020037_ifu(
     input  wire [ 1: 0] rresp,
     input  wire [31: 0] rdata,
     input  wire         rlast,
-    input  wire [ 3: 0] rid,
-
-    input  wire [31: 0] icache_data,
-    input  wire         icache_hit,
-    input  wire         icache_ready,
-    input  wire         mem_req,
-    output reg          icache_req,
-    output reg          mem_ready
+    input  wire [ 3: 0] rid
 );
-    localparam IDLE    = 2'b00;
-    localparam CHECK   = 2'b01;
-    localparam BUSY    = 2'b10;
+    localparam IDLE    = 1'b0;
+    localparam BUSY    = 1'b1;
     
-    reg  [ 1:0] state, next_state;
+    reg         state, next_state;
     reg  [31:0] last_pc;
-    reg         icache_hit_reg;
-
-    assign mem_ready = rvalid && (rresp == 2'b00);
-
-    always @(*) begin
-        case (state)
-            IDLE: begin next_state = (pc != last_pc) ? CHECK : IDLE; end
-            CHECK: begin next_state = mem_req ? BUSY : icache_hit_reg ? IDLE : CHECK; end
-            BUSY: begin next_state = (rvalid && idu_ready && (rresp == 2'b00)) ? IDLE : BUSY; end
-            default: next_state = IDLE;
-        endcase
-    end
-
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             state <= IDLE;
@@ -58,49 +36,42 @@ module ysyx_25020037_ifu(
             rready <= 1'b0;
             last_pc <= 32'h0;
             access_fault <= 1'b0;
-            ifu_valid <= 1'b0;
         end else begin
             state <= next_state;
-            icache_hit_reg <= icache_hit;
+            
             case (state)
                 IDLE: begin
                     if (pc != last_pc) begin
+                        araddr  <= pc;
+                        arvalid <= 1'b1;
                         last_pc <= pc;
-                        icache_req <= 1'b1;
-                    end else begin
-                        icache_req <= 1'b0;
-                    end
-                    arvalid <= 1'b0;
-                    rready <= 1'b0;
-                    ifu_valid <= 1'b0;
-                end
-                CHECK: begin
-                    icache_req <= 1'b0;
-                    if(!icache_req) begin
-                        if (icache_hit_reg) begin
-                            inst <= icache_data;
-                            ifu_valid <= 1'b1;
-                            arvalid <= 1'b0;
-                        end else begin
-                            araddr <= pc;
-                            arvalid <= (~icache_hit_reg & ~icache_hit);
-                        end
+                        rready  <= 1'b0;
                     end
                 end
                 BUSY: begin
                     if (arvalid && arready) begin
+                        //araddr  <= 32'b0;
                         arvalid <= 1'b0;
                         rready <= 1'b1;
                     end
                     if (rvalid && rready && (rresp == 2'b00)) begin
                         inst <= rdata;
-                        ifu_valid <= 1'b1;
                         rready <= 1'b0;
+                    end 
+                    if (rresp != 2'b00) begin
+                        access_fault <= 1'b1;
                     end
                 end
-                default: begin  end
             endcase
         end
+    end
+
+    always @(*) begin
+        case (state)
+            IDLE: next_state = (pc != araddr) ? BUSY : IDLE;        
+            BUSY: next_state = (rvalid && rready) ? IDLE : BUSY;    
+            default: next_state = IDLE;
+        endcase
     end
 
 endmodule
