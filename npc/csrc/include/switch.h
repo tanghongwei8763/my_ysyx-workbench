d50ea5fd1388a8890cf453ca9f8276608a4f9433
tracer-ysyx <tracer@ysyx.org>
2025-08-17 18:16:41 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:16:41 up  7:40,  1 user,  load average: 0.43, 0.26, 0.12

--- npc/csrc/include/switch.h
+++ npc/csrc/include/switch.h
@@ -1,8 +1,8 @@
 #ifndef SWITCH_H
 #define SWITCH_H
 
-#define CONFIG_WAVE 1         //跑大型测试千万不要开启
-#define CONFIG_WATCHPOINT 1
+//#define CONFIG_WAVE 1         //跑大型测试千万不要开启
+//#define CONFIG_WATCHPOINT 1
 //#define CONFIG_PCSRS 1
 //#define CONFIG_ITRACE 1
 //#define CONFIG_MTRACE 1
