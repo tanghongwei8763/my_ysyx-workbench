d2173a61fbd6ad944c8f30ed7954fb32716d46d7
tracer-ysyx <tracer@ysyx.org>
2025-08-19 21:03:53 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:03:53 up  7:10,  1 user,  load average: 0.12, 0.19, 0.13

--- npc/csrc/core/cpu-exec.cpp
+++ npc/csrc/core/cpu-exec.cpp
@@ -269,10 +269,12 @@ static void exec_once() {
     int prev_valid_reg = 0x10;
     do{
 #ifdef CONFIG_DIFFTEST
-        if(((araddr >= 0x10000000) & (araddr <= 0x10000fff) & arvalid) | 
-           ((awaddr >= 0x10000000) & (awaddr <= 0x10000fff) & awvalid)) {difftest_skip_ref();}
-        if(((araddr >= 0x02000000) & (araddr <= 0x0200ffff) & arvalid) |
-           ((awaddr >= 0x02000000) & (awaddr <= 0x0200ffff) & awvalid)) {difftest_skip_ref();}
+        if(((araddr < 0x30000000) | (araddr > 0x3fffffff) & arvalid) & 
+           ((araddr < 0x0f000000) | (araddr > 0x0f002000) & arvalid) & 
+           ((araddr < 0xa0000000) | (araddr > 0xbfffffff) & arvalid)) {difftest_skip_ref();}
+        if(((awaddr < 0x30000000) | (awaddr > 0x3fffffff) & awvalid) & 
+           ((awaddr < 0x0f000000) | (awaddr > 0x0f002000) & awvalid) & 
+           ((awaddr < 0xa0000000) | (awaddr > 0xbfffffff) & awvalid)) {difftest_skip_ref();}
 #endif
         if(prev_valid_reg != prev_valid){
             prev_valid_reg = prev_valid;
