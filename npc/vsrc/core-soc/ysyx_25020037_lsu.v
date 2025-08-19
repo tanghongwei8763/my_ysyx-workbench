ba71c4d32d325b14183f3a4b7036595f31d21da2
tracer-ysyx <tracer@ysyx.org>
2025-08-19 19:03:48 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:03:48 up  5:09,  1 user,  load average: 0.09, 0.09, 0.09

--- npc/vsrc/core-soc/ysyx_25020037_lsu.v
+++ npc/vsrc/core-soc/ysyx_25020037_lsu.v
@@ -166,7 +166,7 @@ module ysyx_25020037_lsu (
                     if (du_to_lu_bus[1]) begin
                         if (arvalid && arready) begin
 `ifdef VERILATOR
-                            if ((addr >= 32'h10000000 && addr <= 32'h10000fff) || (addr >= 32'h02000000 && addr <= 32'h02000ffff)) begin
+                            if (((addr >= 32'h10000000) && (addr <= 32'h10000fff)) || ((addr >= 32'h02000000) && (addr <= 32'h02000ffff))) begin
                                 difftest_skip_ref();
                             end
 `endif
@@ -183,7 +183,7 @@ module ysyx_25020037_lsu (
                     end else if (du_to_lu_bus[0]) begin 
                         if (awvalid && awready && wvalid && wready) begin
 `ifdef VERILATOR
-                            if ((addr >= 32'h10000000 && addr <= 32'h10000fff) || (addr >= 32'h02000000 && addr <= 32'h02000ffff)) begin
+                            if (((addr >= 32'h10000000) && (addr <= 32'h10000fff)) || ((addr >= 32'h02000000) && (addr <= 32'h02000ffff))) begin
                                 difftest_skip_ref();
                             end
 `endif
