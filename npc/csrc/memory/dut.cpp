c8ae9f2f23a4a5f431661350f96899c7664001c6
tracer-ysyx <tracer@ysyx.org>
2025-08-19 16:24:55 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:24:55 up  3:40,  1 user,  load average: 0.10, 0.14, 0.12

--- npc/csrc/memory/dut.cpp
+++ npc/csrc/memory/dut.cpp
@@ -25,7 +25,7 @@ extern Vysyx_25020037 *top;
 
 bool isa_difftest_checkregs(diff_context_t *ref_r, vaddr_t pc) {
 
-  for (int i = 0; i < 32; i++) {
+  for (int i = 0; i < 16; i++) {
     if (ref_r->gpr[i] != dut_gpr[i]) {
       printf("dut->reg[%d]: 0x%08x != ref->gpr[%d]: 0x%08x\n", i, dut_gpr[i], i, ref_r->gpr[i]);
       return false;
