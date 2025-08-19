700304cab91522f83c9ad7a6e49c422b836336a2
tracer-ysyx <tracer@ysyx.org>
2025-08-19 16:18:00 +0800

    > compile NEMU ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:18:00 up  3:33,  1 user,  load average: 0.01, 0.02, 0.11

--- npc/csrc/include/switch.h
+++ npc/csrc/include/switch.h
@@ -4,9 +4,9 @@
 //#define CONFIG_WAVE 1         //跑大型测试千万不要开启
 //#define CONFIG_WATCHPOINT 1
 //#define CONFIG_PCSRS 1
-#define CONFIG_ITRACE 1
+//#define CONFIG_ITRACE 1
 //#define CONFIG_MTRACE 1
 //#define CONFIG_FTRACE 1
-//#define CONFIG_DIFFTEST 1
+#define CONFIG_DIFFTEST 1
 
 #endif
