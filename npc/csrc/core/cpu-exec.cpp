53e119b5d7789f758d429d7f9e315880a3b55eb0
tracer-ysyx <tracer@ysyx.org>
2025-08-21 09:17:23 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:17:23 up 8 min,  1 user,  load average: 0.02, 0.15, 0.10

--- npc/csrc/core/cpu-exec.cpp
+++ npc/csrc/core/cpu-exec.cpp
@@ -268,6 +268,7 @@ static void exec_once() {
     uint64_t clk_sum_reg = 0;
     int prev_valid_reg = 0x10;
     do{
+#ifdef CONFIG_YSYXSOC
 #ifdef CONFIG_DIFFTEST
         if((((araddr < 0x30000000) | (araddr > 0x3fffffff)) & arvalid) & 
            (((araddr < 0x0f000000) | (araddr > 0x0f002000)) & arvalid) & 
@@ -275,6 +276,7 @@ static void exec_once() {
         if((((awaddr < 0x30000000) | (awaddr > 0x3fffffff)) & awvalid) & 
            (((awaddr < 0x0f000000) | (awaddr > 0x0f002000)) & awvalid) & 
            (((awaddr < 0xa0000000) | (awaddr > 0xbfffffff)) & awvalid)) {difftest_skip_ref();}
+#endif
 #endif
         if(prev_valid_reg != prev_valid){
             prev_valid_reg = prev_valid;
