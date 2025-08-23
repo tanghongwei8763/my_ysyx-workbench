45a497382b7d5271bbbfb043dece01b53ba55429
tracer-ysyx <tracer@ysyx.org>
2025-08-23 14:06:20 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:06:20 up 11:58,  1 user,  load average: 0.25, 0.09, 0.02

--- npc/vsrc/core-soc/ysyx_25020037_exu.v
+++ npc/vsrc/core-soc/ysyx_25020037_exu.v
@@ -106,7 +106,7 @@ module ysyx_25020037_exu (
             exu_dnpc_valid <=1'b0;
             exu_dnpc <= 32'b0;
         end else begin
-            exu_valid <= 1'b0;
+            // exu_valid <= 1'b0;
             if(lsu_ready) begin
                 eu_to_ic_bus <= 'b0;
                 eu_to_lu_bus <= 'b0;
