63039d2445c38a6ebd830fdd2989d1fb08650625
tracer-ysyx <tracer@ysyx.org>
2025-08-30 12:23:31 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:23:31 up  2:15,  1 user,  load average: 0.32, 0.19, 0.20

--- npc/vsrc/core-soc/ysyx_25020037.v
+++ npc/vsrc/core-soc/ysyx_25020037.v
@@ -78,8 +78,8 @@ module ysyx_25020037 (
     assign io_slave_rlast   = 1'b0;
     assign io_slave_rid     = 4'b0;
 
-    parameter BLOCK_SIZE   = 32'd8;
-    parameter CACHE_BLOCKS = 32'd8;
+    parameter BLOCK_SIZE   = 32'd16;
+    parameter CACHE_BLOCKS = 32'd4;
 
 `ifdef VERILATOR
     import "DPI-C" function void performance_counter(input int valid, input int type_, input int cache_hit);
