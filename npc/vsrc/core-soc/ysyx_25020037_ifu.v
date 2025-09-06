adf86e03b00846c4244882ddb718baa59faa51ab
tracer-ysyx <tracer@ysyx.org>
2025-09-06 17:18:18 +0800

    > sim RTL ysyx_25020037 唐宏伟 Linux tanghongwei-virtual-machine 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:18:18 up 20:35,  1 user,  load average: 0.18, 0.11, 0.10

--- npc/vsrc/core-soc/ysyx_25020037_ifu.v
+++ npc/vsrc/core-soc/ysyx_25020037_ifu.v
@@ -41,10 +41,11 @@ module ysyx_25020037_ifu #(
 
     localparam OFFSET_WIDTH = $clog2(BLOCK_SIZE);
     localparam TRANSFER_COUNT = BLOCK_SIZE / 4;
-    localparam IDLE    = 1'b0;
-    localparam BUSY    = 1'b1;
+    localparam IDLE    = 2'b00;
+    localparam BUSY    = 2'b01;
+    localparam READ    = 2'b10;
     
-    reg         state, next_state;
+    reg  [ 1:0] state, next_state;
     wire [31:0] pc;
     wire [31:0] inst = fu_to_du_bus[31:0];
     wire [31:0] snpc = pc + 32'h4;
@@ -65,7 +66,8 @@ module ysyx_25020037_ifu #(
     always @(*) begin
         case (state)
             IDLE:  begin next_state = (idu_ready) ? (icache_hit) ? IDLE : BUSY : IDLE; end
-            BUSY:  begin next_state = (mem_ready & idu_ready) ? IDLE : BUSY; end
+            BUSY:  begin next_state = (mem_ready) ? READ : BUSY; end
+            READ:  begin next_state = (idu_ready) ? IDLE : READ; end
             default: next_state = IDLE;
         endcase
     end
@@ -132,9 +134,11 @@ module ysyx_25020037_ifu #(
                             end
                         end
                     end
-                    if(mem_ready & idu_ready) begin
-                        mem_ready <= 1'b0;
-                        mem_data <= 'b0;
+                end
+                READ: begin
+                    mem_ready <= 1'b0;
+                    mem_data <= 'b0;
+                    if (idu_ready) begin
                         fu_to_du_bus <= {pc, icache_data};
                         ifu_valid <= exu_dnpc_valid ? 1'b0 : 1'b1;
                     end
