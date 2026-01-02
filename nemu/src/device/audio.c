23a86bc03e87100cd047cc1997ba1eddf3e05061
Tanghongwei <tanghongwei2024@outlook.com>
2026-01-02 19:40:15 +0800

    NJU-ProjectN/navy-apps ics2024 initialized
    NJU-ProjectN/navy-apps d12fc38844a909502e9270686c04a577a0b85526 Makefile: force to run submake for archives to keep them up-to-date

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -29,13 +29,51 @@ enum {
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
+static uint32_t sbuf_pos = 0;
+
+void sdl_audio_callback(void *userdata, uint8_t *stream, int len){
+  SDL_memset(stream, 0, len);
+  uint32_t used_cnt = audio_base[reg_count];
+  len = len > used_cnt ? used_cnt : len;
+  
+  uint32_t sbuf_size = audio_base[reg_sbuf_size];
+  if( (sbuf_pos + len) > sbuf_size ){
+    SDL_MixAudio(stream, sbuf + sbuf_pos, sbuf_size - sbuf_pos , SDL_MIX_MAXVOLUME);
+    SDL_MixAudio(stream +  (sbuf_size - sbuf_pos), 
+                    sbuf +  (sbuf_size - sbuf_pos), 
+                    len - (sbuf_size - sbuf_pos), 
+                    SDL_MIX_MAXVOLUME);
+  }
+  else 
+    SDL_MixAudio(stream, sbuf + sbuf_pos, len , SDL_MIX_MAXVOLUME);
+  sbuf_pos = (sbuf_pos + len) % sbuf_size;
+  audio_base[reg_count] -= len;
+}
+
+void init_sound() {
+  SDL_AudioSpec s = {};
+  s.format = AUDIO_S16SYS;
+  s.userdata = NULL;
+  s.freq = audio_base[reg_freq];
+  s.channels = audio_base[reg_channels];
+  s.samples = audio_base[reg_samples];
+  s.callback = sdl_audio_callback;
+  SDL_InitSubSystem(SDL_INIT_AUDIO);
+  SDL_OpenAudio(&s, NULL);
+  SDL_PauseAudio(0);
+}
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  if(audio_base[reg_init]==1){
+    init_sound();
+    audio_base[reg_init] = 0;
+  }
 }
 
 void init_audio() {
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
   audio_base = (uint32_t *)new_space(space_size);
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
 #else
